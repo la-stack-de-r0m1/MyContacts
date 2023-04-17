1c7c754810b81efdd6e890e2d5fee4f10aa66c8d
r0m1 <romain.bertholon@gmail.com>
2023-04-17 15:25:14 +0200

    * document some class

--- include/Category.h
+++ include/Category.h
@@ -7,23 +7,45 @@
 namespace MyContacts
 {
 
+/**
+* The contact details of a single contact are set to 'categories'.
+* For example, e-mail address and phone number can go to the 'Contact details'
+* category, while the professional and personal addresses might go to an
+* 'addresses' category.
+*
+* Each contact detail belongs to a category, and the default category is Other.
+*/
 class Category
 {
     std::string categoryName;
     std::map<std::string, std::string> contactDetails;
 
     public:
+        /**
+        * Default category's name ('Other').
+        */
         static const std::string DefaultCategory;
 
     public:
         Category(const std::string& categoryName);
 
+        /**
+        * return the current category name.
+        */
         inline const std::string& name() const
         {
             return categoryName;
         };
 
+        /**
+        * create a new contact detail in the category whose name is `name` and
+        * value is `value`.
+        */
         void setContactDetail(const std::string& name, const std::string& value);
+
+        /**
+        * return the value of the contact detail whose name is `name`.
+        */
         const std::string& getContactDetail(const std::string& name) const;
 };
 
--- include/ContactDetail.h
+++ include/ContactDetail.h
@@ -6,6 +6,14 @@
 namespace MyContacts
 {
 
+/**
+* A contact detail.
+*
+* Each contact detail can have a label. For example, an e-mail can be 'personal' or
+* 'professional'. Idem for the addresses.
+*
+* A contact detail has at least a name and a value; the label can be left empty.
+*/
 class ContactDetail
 {
     std::string detailName;
@@ -15,6 +23,7 @@ class ContactDetail
     public:
         ContactDetail(const std::string& detailName,
                       const std::string& detailValue);
+
         inline const std::string& getDetailName() const { return detailName; }
         inline void setDetailValue(const std::string& value) { detailValue = value; }
         inline const std::string& getDetailValue() const { return detailValue; }
